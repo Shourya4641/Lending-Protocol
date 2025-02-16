f0146406add1e313943b81b3052582cfef600146
Shourya4641 <shouryasarkar001@gmail.com>
2025-02-16 21:17:52 +0530

    updated DecentralizedStableCoin file

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -23,17 +23,53 @@
 
 pragma solidity 0.8.24;
 
+import {ERC20Burnable, ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
 /**
  * @title DecentralizedStableCoin
  * @author Shourya Sarkar
  * Collatorals: Exogenous (ETH and BTC)
  * Minting: Algorithmic
  * Relative Stability: Pegged to USD
- * 
- * This is a contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system. 
+ *
+ * This is a contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
  */
-contract DecentralizedStableCoin {
-    constructor() {
-        
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender){}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint balance = balanceOf(msg.sender);
+
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+
+        if (_amount > balance) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+
+        _mint(_to, _amount);
+
+        return true;
     }
 }
