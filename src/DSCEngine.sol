983509e753ff139acf624a5bb12ff12f4fa73ab3
Shourya4641 <shouryasarkar001@gmail.com>
2025-02-18 09:21:02 +0530

    Deposit collateral function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -23,29 +23,93 @@
 
 pragma solidity 0.8.24;
 
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 /**
  * @title DSCEngine
  * @author Shourya
- *  
+ *
  * The system is designed to be as minimal as possible, and have the tokens maintain a 1:1 ratio with respect to the dollars.
  * This stablecoin has properties
  * - Exogenous collateral
  * - Dollar pegged
  * - Algorithmically stable
- * 
+ *
  * Our DSC system shuold always be overcollaterized. At no point, should the value of all colateral <= the $backed value of all the DSC.
- * 
+ *
  * @notice This contract is the core of the DSC system. It handles all the logic for mining and redeeming DSC, as well as depositing and withdrawing collateral.
  * @notice This contract is loosely based on the MakerDAO DSS (DAI) system
  */
-contract DSCEngine {
+contract DSCEngine is ReentrancyGuard {
+    //////////////////////////// ERRORS ///////////////////////////////
+    error DSCEngine__MustBeMoreThanZero();
+    error DSCEngine__tokenAddressesAndPriceFeedAddressesLengthMustBeSame();
+    error DSCEngine__NotAllowedToken();
+    error DSCEngine__TransferFailed();
 
+    //////////////////////////// STATE VARIABLES ///////////////////////////////
+    mapping(address token => address priceFeed) private s_priceFeeds;
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
+
+    DecentralizedStableCoin private immutable i_dsc;
+
+    //////////////////////////// EVENTS ///////////////////////////////
+    event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
+
+    //////////////////////////// MODIFIERS ////////////////////////////
+    modifier moreThanZero(uint256 _amount) {
+        if (_amount == 0) {
+            revert DSCEngine__MustBeMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedTokens(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
+        _;
+    }
+
+    //////////////////////////// FUNCTIONS /////////////////////////////
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__tokenAddressesAndPriceFeedAddressesLengthMustBeSame();
+        }
+
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+
+    //////////////////////////// EXTERNAL FUNCTIONS /////////////////////////////
     function depositCollateralAndMintDsc() external {}
 
-    function depositCollateral() external {}
+    /**
+     *
+     * @param tokenCollateralAddress The address of the token to deposit as collateral
+     * @param collateralAmount The amount of collateral to deposit
+     */
+    function depositCollateral(address tokenCollateralAddress, uint256 collateralAmount)
+        external
+        moreThanZero(collateralAmount)
+        isAllowedTokens(tokenCollateralAddress)
+        nonReentrant
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += collateralAmount;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, collateralAmount);
+        bool success = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), collateralAmount);
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
+    }
 
     function redeemCollateralForDsc() external {}
-    
+
     function redeemCollateral() external {}
 
     function mintDsc() external {}
@@ -55,6 +119,4 @@ contract DSCEngine {
     function liquidate() external {}
 
     function getHealthFactor() external view {}
-
-
 }
